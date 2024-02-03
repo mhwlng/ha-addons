6490d4a9f07729fb4f9c7840dfe753fb0de5af18
mhwlng <mhwlng@gmail.com>
2024-02-03 12:40:01 +0100

    Update oled.py

--- oled/rootfs/usr/bin/oled.py
+++ oled/rootfs/usr/bin/oled.py
@@ -65,8 +65,14 @@ device.contrast(50)
 
 #fnt = ImageFont.truetype("/usr/bin/SF-Compact.ttf", 12)
 #fnt = ImageFont.truetype("/usr/bin/DejaVuSans.ttf", 13)
-
-fnt = ImageFont.truetype("/usr/bin/FreeSans.ttf", 14)
+#fnt = ImageFont.truetype("/usr/bin/FreeSans.ttf", 14)
+#fnt = ImageFont.truetype("/usr/bin/pixelmix.ttf", 14)
+#fnt = ImageFont.truetype("/usr/bin/miscfs_.ttf", 14)
+#fnt = ImageFont.truetype("/usr/bin/fontawesome-webfont.ttf", 14)
+#fnt = ImageFont.truetype("/usr/bin/Volter__28Goldfish_29.ttf", 14)
+#fnt = ImageFont.truetype("/usr/bin/ProggyTiny.ttf", 14)
+#fnt = ImageFont.truetype("/usr/bin/FreePixel.ttf", 14)
+#fnt = ImageFont.truetype("/usr/bin/DejaVuSansMono.ttf", 14)
 
 # Draw some text
 with canvas(device) as draw:
